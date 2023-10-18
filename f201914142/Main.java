e881fd03aa8d1c929b90a69f3bdeb76a871bcead
Cha, Jiyong <wldyd2113@gmail.com>
2023-10-18 10:39:16 +0900

    two

--- f201914142/Main.java
+++ f201914142/Main.java
@@ -2,6 +2,7 @@ package f201914142;
 
 public class Main {
      public static void main(String[] args) { 
+        Two.print();
         System.out.println("one 201514199"); 
     } 
 }
---
+++ f201914142/Two.java
@@ -0,0 +1,6 @@
+package f201914142; 
+public class Two { 
+    public static void print() { 
+        System.out.println("two"); 
+    } 
+}
