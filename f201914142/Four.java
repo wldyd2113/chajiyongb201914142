6e0f7aa1be50c2de7cbd6400864011ff9d340c5a
Cha, Jiyong <wldyd2113@gmail.com>
2023-10-18 10:45:46 +0900

    four

---
+++ f201914142/Four.java
@@ -0,0 +1,6 @@
+package f201914142; 
+public class Four { 
+    public static void print() { 
+        System.out.println("four"); 
+    } 
+}
--- f201914142/Main.java
+++ f201914142/Main.java
@@ -3,6 +3,7 @@ package f201914142;
 public class Main {
      public static void main(String[] args) { 
         Two.print();
-        System.out.println("one 201514199"); 
+        System.out.println("one 201514199");
+        Four.print();
     } 
 }
